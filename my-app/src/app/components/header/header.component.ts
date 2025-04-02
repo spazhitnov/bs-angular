8e88dc53b2f59be5172ad38a54fe0abd620cb417
spazhitnov <spazhitnov@diasoft.ru>
2025-04-02 14:13:48 +0300

    module1 dz

---
+++ my-app/src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../logo/logo.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, LogoComponent],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
